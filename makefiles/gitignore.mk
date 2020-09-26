c79f996b8b6374c027f757dc99cb7e200dbe9ea3
sunakan <sunakan03@gmail.com>
2020-09-26 08:32:04 +0000

    不要な箇所の削除

--- makefiles/gitignore.mk
+++ makefiles/gitignore.mk
@@ -17,7 +17,6 @@ GIT_IGNORE_URL  := https://www.toptal.com/developers/gitignore/api/$(IGNORE_LIST
 .PHONY: setup-gitignore
 setup-gitignore: .gitignore ## .gitignoreをsetup
 	@make --no-print-directory add-my-go-env-for-gitignore
-	@make --no-print-directory add-rq-for-gitignore
 
 .PHONY: add-my-go-env-for-gitignore
 add-my-go-env-for-gitignore:
@@ -27,7 +26,3 @@ add-my-go-env-for-gitignore:
 	grep '^.cache$$' .gitignore || echo '.cache' >> .gitignore
 	grep '^.config$$' .gitignore || echo '.config' >> .gitignore
 	grep '^__debug_bin$$' .gitignore || echo '__debug_bin' >> .gitignore
-
-.PHONY: add-rq-for-gitignore
-add-rq-for-gitignore:
-	grep 'rq' .gitignore || echo 'rq' >> .gitignore
